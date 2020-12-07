8cbaf06961b73dbcb389d34e8a3d1583f10fd0a4
zhengzhaosheng <1350111595@qq.com>
2020-12-07 20:13:54 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+Page({
+  //跳转至双人对战界面
+  doublePK() {
+  
+  },
+  //跳转至多人选胜界面
+  multipleChoseWinner() {
+    
+  },
+  //跳转至多人选负界面
+  multipleChoseLoser() {
+    
+  },
+  //跳转至人机对战界面
+  manVsMachine() {
+    
+  }
+})
